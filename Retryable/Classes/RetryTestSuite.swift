3201ee7257460d6d85380f37f9f6e81fe13d5108
Kane Cheshire <kane.cheshire@googlemail.com>
2019-06-17 08:42:15 +0100

    Fixes Xcode 11 grouping same suite names under one section

--- Retryable/Classes/RetryTestSuite.swift
+++ Retryable/Classes/RetryTestSuite.swift
@@ -12,8 +12,7 @@ final class RetryTestSuite: XCTestSuite {
 	///
 	/// - Parameter failures: The failures to configure the test suite with.
 	init(_ failures: Set<RetryableTestCase>) {
-		let testsString = failures.count == 1 ? "test" : "tests"
-		super.init(name: "Retrying \(failures.count) failed \(testsString)")
+		super.init(name: .retryableTestSuiteName(from: failures))
 		failures.sorted(by: { $0.name < $1.name }).forEach { failure in
 			guard let selector = failure.invocation?.selector else { fatalError("Tests for \(failure.name) should have a selector") }
 			let test = type(of: failure).init(selector) // Creates a new instance of the test case for the selector that failed
@@ -22,4 +21,19 @@ final class RetryTestSuite: XCTestSuite {
 		}
 	}
 	
+}
+
+private extension String {
+    
+    static func retryableTestSuiteName(from failures: Set<RetryableTestCase>) -> String {
+        let types = failures.reduce(into: "", { result, failure in
+            guard !result.contains("\(type(of: failure))") else { return }
+            if !result.isEmpty {
+                result += ", "
+            }
+            result += "\(type(of: failure))"
+        })
+        return "Retrying \(types)"
+    }
+    
 }
